b78a88d588a409586cfdba7229d5d981efd41b64
Suchitrarathore01 <suchitrarathore01@gmail.com>
2025-06-27 21:22:03 +0530

    assignment_8

---
+++ assignment_8.py
@@ -0,0 +1,28 @@
+import numpy as np
+#question 1 of assignment 8
+a=np.random.rand()
+print(a)
+b=np.empty((4,2))
+print(b)
+c=np.zeros((3,5))
+print(c)
+d=np.ones((4,3,2))
+print(d)
+#question 2 of assignment 8
+e=np.array([[2,3,4],[5,6,7],[8,9,10]])
+print(e)
+#question 3 of assignment 8
+null_v=np.zeros(10)
+null_v[6]=11
+print(null_v)
+#question 4 of assignment 8
+f=np.array([1,2,3,4,5])
+print(f)
+print(f[::-1])
+#question 5 of assignment 8
+g=np.array([1,90,78,6],dtype='f')
+print(g)
+
+
+
+
